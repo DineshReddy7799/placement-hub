1104ce6011b63c30a870b09b5fffba4a12f1a35f
dinesh <224g1a3218@srit.ac.in>
2026-06-08 11:51:19 +0530

    fix: resolve workspace root conflicts and isolate lockfile boundaries

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Core Config Options */
+  experimental: {
+    // Forces Turbopack to explicitly stay inside your project folder
+    turbopack: {
+      root: __dirname,
+    }
+  }
 };
 
 export default nextConfig;
